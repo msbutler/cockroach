1dd1229618d137f3feb6ef1eb2b5aacba17851a0
Michael Butler <butler@cockroachlabs.com>
2024-02-22 10:49:44 -0700

    address steven comments

--- pkg/cmd/roachtest/tests/jobs.go
+++ pkg/cmd/roachtest/tests/jobs.go
@@ -12,6 +12,7 @@ package tests
 
 import (
 	"context"
+	gosql "database/sql"
 	"fmt"
 	"math/rand"
 	"time"
@@ -22,6 +23,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
@@ -37,13 +39,14 @@ var (
 	showJobsTimeout           = time.Minute * 2
 	pollerMinFrequencySeconds = 30
 	roachtestTimeout          = time.Minute * 45
+	workloadDuration          = roachtestTimeout - time.Minute*10
 )
 
 func registerJobs(r registry.Registry) {
 	jobsSpec := r.MakeClusterSpec(nodeCount)
 
 	r.Add(registry.TestSpec{
-		Name:              "jobs/contention",
+		Name:              "jobs/stress",
 		Owner:             registry.OwnerDisasterRecovery,
 		Cluster:           jobsSpec,
 		EncryptionSupport: registry.EncryptionMetamorphic,
@@ -51,26 +54,33 @@ func registerJobs(r registry.Registry) {
 		CompatibleClouds:  registry.OnlyGCE,
 		Suites:            registry.Suites(registry.Nightly),
 		Timeout:           roachtestTimeout,
-		Run:               runJobsContention,
+		Run:               runJobsStress,
 	})
 }
 
-// runJobsContention spins up a cluster to run a large number of jobs with
+// runJobsStress spins up a cluster to run a large number of jobs with
 // frequent interactions to the job system. The test also runs SHOW JOBS
 // periodically to assert it returns in a reasonable amount of itme.
-func runJobsContention(ctx context.Context, t test.Test, c cluster.Cluster) {
+func runJobsStress(ctx context.Context, t test.Test, c cluster.Cluster) {
 	c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings())
 	conn := c.Conn(ctx, t.L(), 1)
 	defer conn.Close()
 
 	if c.IsLocal() {
-		tableCount = 10
-		showJobsTimeout = 10 * time.Second
+		tableCount = 250
+		showJobsTimeout = 30 * time.Second
 		pollerMinFrequencySeconds = 5
+		workloadDuration = time.Minute * 5
+
 	}
 	sqlDB := sqlutils.MakeSQLRunner(conn)
 	sqlDB.Exec(t, "CREATE DATABASE d")
 	sqlDB.Exec(t, "SET DATABASE=d")
+
+	// Because this roachtest spins up and pauses/cancels 5k changefeed jobs
+	// really quickly, run the adopt interval which by default only runs every 30s
+	// and adopts 10 jobs at a time.
+	sqlDB.Exec(t, "SET CLUSTER SETTING jobs.registry.interval.adopt='5s'")
 
 	rng, seed := randutil.NewPseudoRand()
 	t.L().Printf("Rand seed: %d", seed)
@@ -82,10 +92,6 @@ func runJobsContention(ctx context.Context, t test.Test, c cluster.Cluster) {
 	m.Go(func(ctx context.Context) error {
 		defer close(done)
 		testTimer := timeutil.NewTimer()
-		workloadDuration := roachtestTimeout - time.Minute*10
-		if c.IsLocal() {
-			workloadDuration = time.Minute * 1
-		}
 		testTimer.Reset(workloadDuration)
 		select {
 		case <-earlyExit:
@@ -158,28 +164,47 @@ func createTablesWithChangefeeds(
 	t.L().Printf("Finished creating tables with changefeeds")
 }
 
+// checkJobQueryLatency asserts that the queries "SHOW JOBS" and 'SHOW JOB
+// $changefeedJob" run under a specific latency.
 func checkJobQueryLatency(
 	ctx context.Context, t test.Test, c cluster.Cluster, rng *rand.Rand,
 ) error {
 	conn := c.Conn(ctx, t.L(), rng.Intn(nodeCount)+1)
 	defer conn.Close()
+	if err := checkQueryLatency(ctx, "SHOW JOBS", conn, t.L(), showJobsTimeout); err != nil {
+		return err
+	}
+
+	var randomChangefeedJobID int
+	if err := conn.QueryRowContext(ctx, "SELECT job_id FROM [SHOW JOBS] ORDER BY random() LIMIT 1").Scan(&randomChangefeedJobID); err != nil {
+		return err
+	}
+
+	if err := checkQueryLatency(ctx, fmt.Sprintf("SHOW JOB %d", randomChangefeedJobID), conn, t.L(), showJobsTimeout/10); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkQueryLatency(
+	ctx context.Context, query string, conn *gosql.DB, l *logger.Logger, timeout time.Duration,
+) error {
 	queryBegin := timeutil.Now()
 	var err error
-	if err := timeutil.RunWithTimeout(ctx, "show-jobs", showJobsTimeout, func(ctx context.Context) error {
-		_, err = conn.ExecContext(ctx, "SHOW JOBS")
+	if err := timeutil.RunWithTimeout(ctx, query, timeout, func(ctx context.Context) error {
+		_, err = conn.ExecContext(ctx, query)
 		return err
 	}); err != nil {
-		t.L().Printf("Show jobs query exceeded max latency of %.2f. Ran in %.2f. Try to grab explain analyze", showJobsTimeout, timeutil.Since(queryBegin).Seconds())
-		explainErr := timeutil.RunWithTimeout(ctx, "show-jobs-explain", showJobsTimeout*10, func(ctx context.Context) error {
+		l.Printf("%s query exceeded max latency of %.2f seconds. Ran in %.2f seconds. Try to grab explain analyze", query, timeout.Seconds(), timeutil.Since(queryBegin).Seconds())
+		explainErr := timeutil.RunWithTimeout(ctx, fmt.Sprintf("%s explain", query), timeout*10, func(ctx context.Context) error {
 			var explainAnalyze string
-			explainErr := conn.QueryRowContext(ctx, "EXPLAIN ANALYZE (VERBOSE) SHOW JOBS").Scan(&explainAnalyze)
-			t.L().Printf("%s", explainAnalyze)
+			explainErr := conn.QueryRowContext(ctx, fmt.Sprintf("EXPLAIN ANALYZE (VERBOSE) %s", query)).Scan(&explainAnalyze)
+			l.Printf("%s", explainAnalyze)
 			return explainErr
 		})
 		return errors.CombineErrors(err, explainErr)
 	}
-	t.L().Printf("SHOW JOBS ran in %.2f seconds", timeutil.Since(queryBegin).Seconds())
-	// TODO(msbutler): once SHOW JOB ID no longer conducts a full scan, test its latency on a random changefeed job.
+	l.Printf("%s ran in %.2f seconds", query, timeutil.Since(queryBegin).Seconds())
 	return nil
 }
 
@@ -208,7 +233,6 @@ func pauseResumeChangefeeds(
 	if len(jobs) < tableCount/10 {
 		return nil
 	}
-
 	jobAction := func(cmd string, count int) {
 		errCount := 0
 		for i := 0; i < count; i++ {
@@ -254,17 +278,9 @@ func checkJobSystemHealth(ctx context.Context, t test.Test, c cluster.Cluster, r
 	// TODO(msbutler): once we speed up cancel-requested processing, we ought to
 	// assert that relatively jobs are in the cancel requested state.
 	var canceledChangefeedCount int
-	sqlDB.QueryRow(t, `SELECT count(*) FROM system.jobs WHERE job_type='CHANGEFEED' AND status='canceled';`).Scan(&canceledChangefeedCount)
+	sqlDB.QueryRow(t, `SELECT count(*) FROM system.jobs WHERE job_type='CHANGEFEED' AND status='canceled'`).Scan(&canceledChangefeedCount)
 	if canceledChangefeedCount == 0 {
 		failedAssertions = append(failedAssertions, "No changefeed jobs were in the canceled state")
-	}
-
-	// Check That the Poller job has run at least once. We expect it to run quite slowly, but hopefully it's run at least once.
-	var lastRun time.Time
-	sqlDB.QueryRow(t, "SELECT last_run FROM system.jobs WHERE job_type = 'POLL JOBS STATS';").Scan(&lastRun)
-	t.L().Printf("Last run Poller Run %s", lastRun)
-	if lastRun.Compare(timeutil.Now().Add(-time.Minute*40)) < 0 {
-		failedAssertions = append(failedAssertions, fmt.Sprintf("last POLL JOB STATS update %s is over 40 minutes old", lastRun))
 	}
 
 	require.Equal(t, 0, len(failedAssertions), failedAssertions)
