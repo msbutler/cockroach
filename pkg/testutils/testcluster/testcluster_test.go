b4af25e7edc067ddb28881003c9c20264f3af16c
Michael Butler <butler@cockroachlabs.com>
2025-02-17 11:04:08 -0700

    disable tls for http

--- pkg/testutils/testcluster/testcluster_test.go
+++ pkg/testutils/testcluster/testcluster_test.go
@@ -41,6 +41,7 @@ func TestClusterStart(t *testing.T) {
 
 // Stats over 100 runs: max = 6.3s, min = 2.0s, avg = 3.1s, dev = 1.1s
 // DisableEventLogging: Stats over 100 runs: max = 3.6s, min = 1.7s, avg = 3.1s, dev = 0.3s
+// Hard disable: Stats over 100 runs: max = 3.4s, min = 1.7s, avg = 3.0s, dev = 0.3s
 func TestServerStartWithExternalTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -54,6 +55,7 @@ func TestServerStartWithExternalTenant(t *testing.T) {
 
 // Stats over 100 runs: max = 6.2s, min = 2.1s, avg = 2.8s, dev = 1.0s
 // DisableEventLogging: Stats over 100 runs: max = 3.3s, min = 1.5s, avg = 2.9s, dev = 0.3s
+// Hard Disable: Stats over 100 runs: max = 3.2s, min = 1.5s, avg = 2.9s, dev = 0.3s
 
 func TestServerStartWithSharedTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
@@ -68,6 +70,7 @@ func TestServerStartWithSharedTenant(t *testing.T) {
 
 // Stats over 100 runs: max = 5.3s, min = 1.1s, avg = 2.3s, dev = 1.1s
 // DisableEventLogging: Stats over 100 runs: max = 2.6s, min = 1.2s, avg = 2.2s, dev = 0.2s
+// Hard disable:  Stats over 100 runs: max = 2.8s, min = 1.2s, avg = 2.2s, dev = 0.2s
 func TestServerStartNoTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -75,6 +78,7 @@ func TestServerStartNoTenant(t *testing.T) {
 	srv, _, _ := serverutils.StartServer(t, base.TestServerArgs{
 		DefaultTestTenant: base.TestControlsTenantsExplicitly,
 		DisableEventLog:   true,
+		DisableTLSForHTTP: true,
 	})
 	srv.Stopper().Stop(context.Background())
 }
