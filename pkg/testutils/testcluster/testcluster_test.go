e2b45cfa907e5101a66bab872d2ed20c5c7db402
Michael Butler <butler@cockroachlabs.com>
2025-02-17 10:42:37 -0700

    actually disable event logs

--- pkg/testutils/testcluster/testcluster_test.go
+++ pkg/testutils/testcluster/testcluster_test.go
@@ -32,6 +32,7 @@ import (
 )
 
 // Stats over 100 runs: max = 7.1s, min = 3.2s, avg = 6.3s, dev = 0.7s
+// Disable EventLogging: Stats over 100 runs: max = 6.7s, min = 3.1s, avg = 6.0s, dev = 0.6s
 func TestClusterStartNoTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -39,6 +40,7 @@ func TestClusterStartNoTenant(t *testing.T) {
 	tc := StartTestCluster(t, 3, base.TestClusterArgs{
 		ServerArgs: base.TestServerArgs{
 			DefaultTestTenant: base.TestControlsTenantsExplicitly,
+			DisableEventLog:   true,
 		},
 	})
 	defer tc.Stopper().Stop(context.Background())
@@ -46,6 +48,7 @@ func TestClusterStartNoTenant(t *testing.T) {
 
 // Stats over 100 runs: max = 6.3s, min = 2.0s, avg = 3.1s, dev = 1.1s
 // DisableEventLogging: Stats over 100 runs: max = 3.6s, min = 1.7s, avg = 3.1s, dev = 0.3s
+// Hard disable: Stats over 100 runs: max = 3.4s, min = 1.7s, avg = 3.0s, dev = 0.3s
 func TestServerStartWithExternalTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -59,6 +62,7 @@ func TestServerStartWithExternalTenant(t *testing.T) {
 
 // Stats over 100 runs: max = 6.2s, min = 2.1s, avg = 2.8s, dev = 1.0s
 // DisableEventLogging: Stats over 100 runs: max = 3.3s, min = 1.5s, avg = 2.9s, dev = 0.3s
+// Hard Disable: Stats over 100 runs: max = 3.2s, min = 1.5s, avg = 2.9s, dev = 0.3s
 
 func TestServerStartWithSharedTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
@@ -73,6 +77,7 @@ func TestServerStartWithSharedTenant(t *testing.T) {
 
 // Stats over 100 runs: max = 5.3s, min = 1.1s, avg = 2.3s, dev = 1.1s
 // DisableEventLogging: Stats over 100 runs: max = 2.6s, min = 1.2s, avg = 2.2s, dev = 0.2s
+// Hard disable:  Stats over 100 runs: max = 2.8s, min = 1.2s, avg = 2.2s, dev = 0.2s
 func TestServerStartNoTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
