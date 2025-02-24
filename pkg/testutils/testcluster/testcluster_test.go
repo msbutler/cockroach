a8c3fe1092fabe77093082108e70ad0672663817
Michael Butler <butler@cockroachlabs.com>
2025-02-24 08:24:04 -0700

    skip some perm upgrades

--- pkg/testutils/testcluster/testcluster_test.go
+++ pkg/testutils/testcluster/testcluster_test.go
@@ -49,6 +49,7 @@ func TestClusterStartNoTenant(t *testing.T) {
 // Stats over 100 runs: max = 6.3s, min = 2.0s, avg = 3.1s, dev = 1.1s
 // DisableEventLogging: Stats over 100 runs: max = 3.6s, min = 1.7s, avg = 3.1s, dev = 0.3s
 // Hard disable: Stats over 100 runs: max = 3.4s, min = 1.7s, avg = 3.0s, dev = 0.3s
+// Remove some perm upgrades: Stats over 100 runs: max = 2.9s, min = 1.3s, avg = 2.6s, dev = 0.3s
 func TestServerStartWithExternalTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -63,6 +64,7 @@ func TestServerStartWithExternalTenant(t *testing.T) {
 // Stats over 100 runs: max = 6.2s, min = 2.1s, avg = 2.8s, dev = 1.0s
 // DisableEventLogging: Stats over 100 runs: max = 3.3s, min = 1.5s, avg = 2.9s, dev = 0.3s
 // Hard Disable: Stats over 100 runs: max = 3.2s, min = 1.5s, avg = 2.9s, dev = 0.3s
+// Remove some perm upgrades: Stats over 100 runs: max = 3.0s, min = 1.4s, avg = 2.5s, dev = 0.2s
 
 func TestServerStartWithSharedTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
@@ -78,6 +80,8 @@ func TestServerStartWithSharedTenant(t *testing.T) {
 // Stats over 100 runs: max = 5.3s, min = 1.1s, avg = 2.3s, dev = 1.1s
 // DisableEventLogging: Stats over 100 runs: max = 2.6s, min = 1.2s, avg = 2.2s, dev = 0.2s
 // Hard disable:  Stats over 100 runs: max = 2.8s, min = 1.2s, avg = 2.2s, dev = 0.2s
+// Remove some perm upgrades: Stats over 100 runs: max = 2.2s, min = 1.0s, avg = 1.9s, dev = 0.2s
+
 func TestServerStartNoTenant(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
