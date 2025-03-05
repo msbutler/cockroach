fa61e1c07f6c1c7fa7bb29124037b1cdbb999f46
Michael Butler <butler@cockroachlabs.com>
2025-03-05 11:56:25 -0500

    crosscluster/physical: auth ALTER REPLICATION JOB with REPLICATIONDEST
    Epic CRDB-47102
    
    Release note (ops change, backwards incompatible change): all ALTER REPLICATION
    JOB cmds, including cutover, require the REPLICATIONDEST priv, in addition to
    MANAGEVIRTUALCLUSTER.

--- pkg/crosscluster/physical/alter_replication_job_test.go
+++ pkg/crosscluster/physical/alter_replication_job_test.go
@@ -608,9 +608,21 @@ func TestAlterReplicationJobErrors(t *testing.T) {
 
 	db := sqlutils.MakeSQLRunner(sqlDB)
 
+	//create test user
+
 	t.Run("alter tenant subqueries", func(t *testing.T) {
 		// Regression test for #136339
 		db.ExpectErr(t, "subqueries are not allowed", "ALTER TENANT (select 't2') START REPLICATION OF t1 ON 'foo'")
+	})
+	t.Run("alter replication privs", func(t *testing.T) {
+		db.Exec(t, fmt.Sprintf("CREATE USER %s", username.TestUser))
+		testuser := sqlutils.MakeSQLRunner(srv.SQLConn(t, serverutils.User(username.TestUser)))
+		testuser.ExpectErr(t, "user testuser does not have MANAGEVIRTUALCLUSTER system privilege", "ALTER TENANT t1 START REPLICATION OF t1 ON 'foo'")
+		db.Exec(t, fmt.Sprintf("GRANT SYSTEM MANAGEVIRTUALCLUSTER TO %s", username.TestUser))
+		testuser.ExpectErr(t, "user testuser does not have REPLICATIONDEST system privilege", "ALTER TENANT t1 START REPLICATION OF t1 ON 'foo'")
+		db.Exec(t, fmt.Sprintf("GRANT SYSTEM REPLICATIONDEST TO %s", username.TestUser))
+		// Implies we got past the priv checks.
+		testuser.ExpectErr(t, `tenant "t1" does not exist`, "ALTER TENANT t1 START REPLICATION OF t1 ON 'foo'")
 	})
 
 }
