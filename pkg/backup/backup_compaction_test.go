79282fad689b6346a67d4283c138d200e60989de
Michael Butler <butler@cockroachlabs.com>
2025-03-25 15:57:11 -0400

    exp

--- pkg/backup/backup_compaction_test.go
+++ pkg/backup/backup_compaction_test.go
@@ -428,8 +428,6 @@ func TestScheduledBackupCompaction(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
-	skip.WithIssue(t, 143394, "flaky test")
-
 	ctx := context.Background()
 	th, cleanup := newTestHelper(t)
 	defer cleanup()
